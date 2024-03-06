725f4886c56ecdbd31c45fd26e96dcac2286e43e
DGSGROUP\nicola.sacco <nicola.sacco@dgsspa.com>
2024-03-06 15:41:50 +0100

    add api change

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -494,6 +494,117 @@ export const downloadNotifchePagoPa = async (token:string, nonce:string , body: 
 };
 
 
+export const getTipologieScadenziario = async (token:string, nonce:string) => {
+    const response =  await axios.get(`${url}/api/tipologia/scadenziariocontestazioni?nonce=${nonce}`,
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },}
+    );
+
+    return response;
+};
+
+
+
+
+export const getListaRel = async (token:string, nonce:string , page:number, pageSize:number, body: BodyRel) => {
+    const response =  await axios.post(`${url}/api/rel/ente?page=${page}&pageSize=${pageSize}&nonce=${nonce}`,
+        body,
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },
+        }
+    );
+    return response;
+};
+
+export const getSingleRel = async (token:string, nonce:string , id:string) => {
+    const response =  await axios.get(`${url}/api/rel/ente/${id}?nonce=${nonce}`,
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },}
+    );
+
+    return response;
+};
+
+export const getRelPdf = async ( token:string ,nonce:string , id:string) => {
+    const response =  await axios.get(`${url}/api/rel/ente/download/${id}?nonce=${nonce}`,  
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },}
+    );
+
+    return response;
+};
+
+export const getRelExel = async ( token:string ,nonce:string , id:string) => {
+    const response =  await axios.get(`${url}/api/rel/ente/righe/${id}?nonce=${nonce}`,  
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },}
+    );
+
+    return response;
+};
+
+export const downloadListaRel = async (token:string, nonce:string , body: BodyRel) => {
+    const response =  await axios.post(`${url}/api/rel/ente/documento/ricerca?&nonce=${nonce}`,
+        body,
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },
+        }
+    );
+    return response;
+};
+
+export const getListaRelPagoPa = async (token:string, nonce:string , page:number, pageSize:number, body: BodyRel) => {
+    const response =  await axios.post(`${url}/api/rel/pagopa?page=${page}&pageSize=${pageSize}&nonce=${nonce}`,
+        body,
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },
+        }
+    );
+    return response;
+};
+
+export const downloadListaRelPagopa = async (token:string, nonce:string , body: BodyRel) => {
+    const response =  await axios.post(`${url}/api/rel/pagopa/documento/ricerca?nonce=${nonce}`,
+        body,
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },
+        }
+    );
+    return response;
+};
+
+export const getRelExelPagoPa = async ( token:string ,nonce:string , id:string) => {
+    const response =  await axios.get(`${url}/api/rel/pagopa/righe/${id}?nonce=${nonce}`,  
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },}
+    );
+
+    return response;
+};
+
+export const getSingleRelPagopa = async ( token:string ,nonce:string , id:string) => {
+    const response =  await axios.get(`${url}/api/rel/pagopa/${id}?nonce=${nonce}`,  
+        { headers: {
+            Authorization: 'Bearer ' + token
+        },}
+    );
+
+    return response;
+};
+
+
+
+
+
 
 
 
