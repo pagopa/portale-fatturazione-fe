165c3f577739b9a44ef6bff332d356c15bc00244
DGSGROUP\nicola.sacco <nicola.sacco@dgsspa.com>
2024-04-18 17:04:59 +0200

    fix nome comune lista datiFat e lista comm if single element

--- src/types/typeListaDatiFatturazione.tsx
+++ src/types/typeListaDatiFatturazione.tsx
@@ -2,11 +2,7 @@ export interface ListaDatiFatturazioneProps{
     mainState:any,
     dispatchMainState:any,
     setBodyGetLista:any,
-    bodyGetLista:{
-        descrizione:string,
-        prodotto:string,
-        profilo:string
-    },
+    bodyGetLista:BodyGetListaDatiFatturazione,
     infoPageListaDatiFat:{
         page: number, 
         pageSize: number
@@ -17,4 +13,31 @@ export interface ResponseDownloadListaFatturazione {
     data:{
         documento:string
     }
+}
+
+export interface BodyGetListaDatiFatturazione {
+    descrizione:string,
+    prodotto:string,
+    profilo:string
+}
+
+export interface GridElementListaFatturazione {
+    key: string,
+    ragioneSociale: string,
+    idEnte: string,
+    prodotto: string,
+    profilo: string,
+    id: number,
+    tipoCommessa:string,
+    cup: string,
+    codCommessa: string,
+    dataDocumento: string,
+    splitPayment: boolean,
+    idDocumento: string,
+    map: string,
+    pec: string,
+    notaLegale: boolean,
+    dataCreazione: string,
+    dataModifica: string
+    
 }
