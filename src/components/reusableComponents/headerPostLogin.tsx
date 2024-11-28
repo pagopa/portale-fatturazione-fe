174a9100ffbdaaaf5ea97d25b214e31303750941
DGSGROUP\nicola.sacco <nicola.sacco@dgsspa.com>
2024-11-28 17:06:58 +0100

    first test download Manuale with external uri

--- src/components/reusableComponents/headerPostLogin.tsx
+++ src/components/reusableComponents/headerPostLogin.tsx
@@ -1,10 +1,11 @@
 import { HeaderAccount } from '@pagopa/mui-italia';
 import { useLocation, useNavigate } from 'react-router';
-import { redirect } from '../../api/api';
+import { getManuale, redirect } from '../../api/api';
 import {useMsal } from '@azure/msal-react';
 import { loginRequest } from '../../authConfig';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GlobalContext } from '../../store/context/globalContext';
+import axios from 'axios';
 
 
 type JwtUser = {
@@ -35,16 +36,75 @@ const HeaderPostLogin = () => {
         email: "",
     };
 
+    const [urlProva, setUrlProva] = useState('');
     // start actions sul manuale operativo , download del manuale
 
-    const onButtonClick = () => {
-        const pdfUrl = "/ManualeUtentePortaleFatturazione5.pdf";
-        const link = document.createElement("a");
-        link.href = pdfUrl;
-        link.download = "ManualeUtentePortaleFatturazione.pdf";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+    useEffect(()=>{
+        if(urlProva !== ''){
+            const link = document.createElement("a");
+            link.href = urlProva;
+            link.download = 'ciao.pdf';
+            document.body.appendChild(link);
+        
+            link.click();
+        
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(urlProva);
+            setUrlProva('');
+        }
+
+    },[urlProva]);
+
+    const onButtonClick = async () => {
+        
+
+        await getManuale().then(async(res) => {
+            try {
+                /* 
+                console.log(res, 'pippo');
+                // setUrlProva(res);
+                const link = document.createElement("a");
+                link.href = res;
+                link.download = 'ciao.pdf';
+                document.body.appendChild(link);
+        
+                link.click();
+        
+                document.body.removeChild(link);
+                window.URL.revokeObjectURL(res);
+              
+                
+                // Make GET request with responseType set to 'blob'
+                await axios.get(res,{
+                    responseType: 'blob', // Ensures the response is treated as binary data
+                    headers: {
+                        'Content-Type': 'application/pdf', // Optional, depending on server
+                    },
+                }).then((response)=>{
+                    console.log(response, 'BLOB');
+                   
+                }).catch((err)=> console.log('errore catch',err,'yyy'));
+  */
+                console.log(res,'RESRES');
+                await fetch(res)  // URL to the API endpoint
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        console.log(response,'mimmo');
+                    })
+                    .then(data => {
+                        return data; // Handle the response data
+                    })
+                    .catch(error => {
+                        console.error('There was a problem with the fetch operation:', error);
+                    });
+     
+            } catch (error) {
+                console.error('Error downloading blob from Azure:', error);
+            }
+        });
+
     };
     //end actions sul manuale operativo , download del manuale
     // start on click su assistenza redirect alla tua apllicazione predefinita per l'invio mail
