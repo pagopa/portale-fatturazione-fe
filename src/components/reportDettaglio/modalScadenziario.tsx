e94eb9cb68bfe24c90f3e4a2c4ab900bbe54c21a
DGSGROUP\nicola.sacco <nicola.sacco@dgsspa.com>
2025-09-04 18:54:22 +0200

    scadenzario layout ok

--- src/components/reportDettaglio/modalScadenziario.tsx
+++ src/components/reportDettaglio/modalScadenziario.tsx
@@ -42,7 +42,7 @@ const ModalScadenziario : React.FC<ModalScadenziario> = ({setOpen, open, nonce,d
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: (profilo.profilo === 'CON' || profilo.profilo === 'REC') ?'80%':'60%',
+        width:"auto",
         bgcolor: 'background.paper',
         boxShadow: 24,
         p: 4,
@@ -94,76 +94,67 @@ const ModalScadenziario : React.FC<ModalScadenziario> = ({setOpen, open, nonce,d
                         </div>
                        
                     </div>
-                    <div className='mt-3'>
-                        <TableContainer component={Paper}>
-                            <Table aria-label="simple table">
-                                <div style={{overflowY: "auto",maxHeight: "75vh"}}>
-                                    <TableHead sx={{ position:'sticky',top: '0',zIndex:'10', backgroundColor:'#fffff'}}>
-                                        <TableRow sx={{ position: "sticky", top: 0, zIndex: 20, backgroundColor: "#fafafa" }}>
-                                            <TableCell colSpan={2} align="center" sx={{ borderLeft: "1px solid black",fontWeight: "bold",borderTop:"1px solid black" }}>
+        
+                    <div className='mt-5'>
+                        <TableContainer  sx={{overflowY: "auto",maxHeight: "75vh"}} component={Paper}>
+                            <Table>
+                                <TableHead sx={{ position:'sticky',top: '0',zIndex:'10', backgroundColor:'#f2f2f2',borderLeft: "10px solid #e0e0e0",borderRight: "10px solid #e0e0e0"}}>
+                                    <TableRow sx={{ position: "sticky", top: 0, zIndex: 20, backgroundColor: "grey.200",borderColor: "divider"}}>
+                                        <TableCell colSpan={2} align="center" sx={{ fontWeight: "bold"}}>
                                                 
-                                            </TableCell>
-                                            <TableCell sx={{
-                                                borderLeft: "1px solid black",
-                                                borderRight: "1px solid black",
-                                                borderTop:"1px solid black",
-                                                fontWeight: "bold"
-                                            }} colSpan={3} align="center">
-                                                Ente/Aderente
-                                            </TableCell>
-                                            <TableCell sx={{borderRight: "1px solid black",fontWeight: "bold",borderTop:"1px solid black"}} colSpan={1} align="center">
-                                                Supporto SEND
-                                            </TableCell>
-                                            {(profilo.profilo === 'CON' || profilo.profilo === 'REC') &&
-                                            <TableCell colSpan={2} align="center" sx={{ fontWeight: "bold",borderLeft: "1px solid black",
-                                                borderRight: "1px solid black",
-                                                borderTop:"1px solid black", }}>
+                                        </TableCell>
+                                        <TableCell sx={{fontWeight: "bold",whiteSpace: "nowrap",borderRight: "10px solid #e0e0e0",borderLeft: "10px solid #e0e0e0"}} colSpan={6} align="center">
+                                                Ente Aderente
+                                        </TableCell>
+                                        <TableCell sx={{fontWeight: "bold",whiteSpace: "nowrap", borderRight: "10px solid e0e0e0"}} colSpan={2} align="center">
+                                                Supporto postalizzazione
+                                        </TableCell>
+                                        {(profilo.profilo === 'CON' || profilo.profilo === 'REC') &&
+                                            <TableCell colSpan={2} align="center" sx={{ fontWeight: "bold",whiteSpace: "nowrap"}}>
                                                 Consolidatore/Recapitista
                                             </TableCell>
-                                            }
-                                        </TableRow>
-                                        <TableRow sx={{ position: "sticky", top: 48, zIndex: 19, backgroundColor: "#fafafa" }}>
-                                            <TableCell  sx={{ borderLeft: "1px solid black",borderBottom:"1px solid black" }} colSpan={1}>Mese</TableCell>
-                                            <TableCell sx={{borderRight: "1px solid black",borderBottom:"1px solid black"}} colSpan={1} >Anno </TableCell>
-                                            <TableCell sx={{borderBottom:"1px solid black"}} colSpan={1} align="left">Inizio inserimento</TableCell>
-                                            <TableCell sx={{borderBottom:"1px solid black"}} colSpan={1} align="left">Fine inserimento</TableCell>
-                                            <TableCell sx={{borderRight: "1px solid black",borderBottom:"1px solid black"}} colSpan={1} align="left">Fine risposta</TableCell>
-                                            <TableCell sx={{borderRight: "1px solid black",borderBottom:"1px solid black"}} colSpan={1} align="left">Fine</TableCell>
-                                            {(profilo.profilo === 'CON' || profilo.profilo === 'REC') &&
+                                        }
+                                    </TableRow>
+                                    <TableRow sx={{ position: "sticky", top: 48, zIndex: 19, backgroundColor: "grey.100",borderColor: "divider"  }}>
+                                        <TableCell   align="center" colSpan={1}>Mese</TableCell>
+                                        <TableCell  sx={{borderRight: "10px solid #e0e0e0"}} align="center" colSpan={1} >Anno </TableCell>
+                                        <TableCell sx={{ whiteSpace: "nowrap"}} colSpan={2} align="center">Inizio inserimento</TableCell>
+                                        <TableCell sx={{ whiteSpace: "nowrap"}} colSpan={2} align="center">Fine inserimento</TableCell>
+                                        <TableCell sx={{ whiteSpace: "nowrap",borderRight: "10px solid #e0e0e0"}} colSpan={2} align="center">Tempo di risposta</TableCell>
+                                        <TableCell sx={{whiteSpace: "nowrap"}} colSpan={2} align="center">Tempo di risposta</TableCell>
+                                        {(profilo.profilo === 'CON' || profilo.profilo === 'REC') &&
                                             <>
-                                                <TableCell  sx={{borderBottom:"1px solid black"}} colSpan={1} align="left">Inizo risposta </TableCell>
-                                                <TableCell sx={{borderRight: "1px solid black",borderBottom:"1px solid black"}} colSpan={1} align="left">Fine risposta</TableCell>
+                                                <TableCell  sx={{whiteSpace: "nowrap"}} colSpan={1} align="center">Inizo risposta </TableCell>
+                                                <TableCell sx={{whiteSpace: "nowrap"}} colSpan={1} align="center">Tempo di risposta</TableCell>
                                             </>
+                                        }
+                                    </TableRow>
+                                </TableHead>
+                                
+                                <TableBody>
+                                    {datiScadenziario.map((row) => (
+                                        <TableRow
+                                            key={row.meseContestazione}
+                                            sx={{ borderLeft: "10px solid #e0e0e0",borderRight: "10px solid #e0e0e0",backgroundColor:"#fcfcfc",borderBottom: "2px solid #e0e0e0"}}
+                                        >
+                                            <TableCell  align="center" scope="row">
+                                                {row.meseContestazione}
+                                            </TableCell>
+                                            <TableCell  sx={{ borderRight: "10px solid #e0e0e0",boxSizing: "border-box",whiteSpace: "nowrap"}} align="center">{row.annoContestazione}</TableCell>
+                                            <TableCell colSpan={2} sx={{whiteSpace: "nowrap"}} align="center">{row.dataInizio}</TableCell>
+                                            <TableCell colSpan={2} sx={{whiteSpace: "nowrap"}} align="center">{row.dataFine}</TableCell>
+                                            <TableCell colSpan={2} sx={{ borderRight: "10px solid #e0e0e0",whiteSpace: "nowrap",boxSizing: "border-box"}}  align="center">{row.chiusuraContestazioni}</TableCell>
+                                            <TableCell colSpan={2} sx={{ borderRight: "10px solid #e0e0e0",whiteSpace: "nowrap",boxSizing: "border-box"}}  align="center">{row.tempoRisposta}</TableCell>
+                                            {(profilo.profilo === 'CON' || profilo.profilo === 'REC') &&
+                                                <>
+                                                    <TableCell sx={{ borderRight: "10px solid #e0e0e0",boxSizing: "border-box",whiteSpace: "nowrap"}} align="center">{row.dataRecapitistaInizio}</TableCell>
+                                                    <TableCell sx={{ borderRight: "10px solid #e0e0e0",boxSizing: "border-box",whiteSpace: "nowrap"}} align="center">{row.dataRecapitistaFine}</TableCell>
+                                                </>
                                             }
                                         </TableRow>
-                                    </TableHead>
-                                
-                                    <TableBody>
-                                        {datiScadenziario.map((row) => (
-                                            <TableRow
-                                                key={row.meseContestazione}
-                                                sx={{ borderLeft: "1px solid black"}}
-                                            >
-                                                <TableCell align="left" scope="row">
-                                                    {row.meseContestazione}
-                                                </TableCell>
-                                                <TableCell sx={{
-                                                    borderRight: "1px solid black",
-                                                }} align="left">{row.annoContestazione}</TableCell>
-                                                <TableCell align="left">{row.dataInizio}</TableCell>
-                                                <TableCell align="left">{row.dataFine}</TableCell>
-                                                <TableCell sx={{borderRight: "1px solid black"}}  align="left">{row.chiusuraContestazioni}</TableCell>
-                                                <TableCell sx={{borderRight: "1px solid black"}}  align="left">{row.tempoRisposta}</TableCell>
-                                                {(profilo.profilo === 'CON' || profilo.profilo === 'REC') &&
-                                                <>
-                                                    <TableCell sx={{borderRight: "1px solid black"}} align="left">{row.dataRecapitistaInizio}</TableCell>
-                                                    <TableCell sx={{borderRight: "1px solid black"}} align="left">{row.dataRecapitistaFine}</TableCell>
-                                                </>
-                                                }
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </div> 
+                                    ))}
+                                </TableBody>
+                              
                             </Table>
                         </TableContainer>
                     </div> 
