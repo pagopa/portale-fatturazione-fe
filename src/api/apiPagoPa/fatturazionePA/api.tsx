48823ec5e6d8bdaf888468dc61200159097252d7
ns-dgsspa <nicola.sacco@dgsspa.com>
2026-05-28 18:30:07 +0200

    add api download non inviate

--- src/api/apiPagoPa/fatturazionePA/api.tsx
+++ src/api/apiPagoPa/fatturazionePA/api.tsx
@@ -352,3 +352,16 @@ export const getDocSospesiExelPagoPa = async ( token:string ,nonce:string, id:st
     return response;
 };
     
+export const downloadReportRelNonFatturate = async (token:string, nonce:string,body: {tipologiaFattura: string[]|null}) => {
+    const response = await fetch(`${url}/api/fatture/pagopa/non-inviate/report?nonce=${nonce}`, 
+        {
+            headers: {
+                Authorization: 'Bearer '+token,
+                'Content-type':'application/json'
+            },
+            method: 'POST',
+            body:JSON.stringify(body),
+        });
+    
+    return response;
+};
