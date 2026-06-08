aee27dc743893f8a25a3c2376f6f65aa7670a244
ns-dgsspa <nicola.sacco@dgsspa.com>
2026-06-08 12:09:21 +0200

    add config file header anagrafica psp

---
+++ src/assets/configurations/conf_GridAnagraficaPsp.tsx
@@ -0,0 +1,14 @@
+export const headerAnagraficaPsp : any[] = [
+  { label: "Nome PSP", align: "center", width: "150px", keyValue: "nomePsp" },
+  { label: "ID Contratto", align: "center", width: "120px", keyValue: "idContratto" },
+  { label: "Trimestre", align: "center", width: "100px", keyValue: "trimestre" },
+  { label: "Nome Fornitore", align: "center", width: "150px", keyValue: "nomeFornitore" },
+  { label: "E-mail PEC", align: "center", width: "150px", keyValue: "emailPec" },
+  { label: "Codice SDI", align: "center", width: "120px", keyValue: "codiceSdi" },
+  { label: "Codice ABI", align: "center", width: "120px", keyValue: "codiceAbi" },
+  { label: "E-Mail Ref. Fattura", align: "center", width: "150px", keyValue: "emailRefFattura" },
+  { label: "Data", align: "center", width: "100px", keyValue: "data" }
+];
+
+
+
